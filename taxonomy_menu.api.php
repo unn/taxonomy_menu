771fc5a0d8fddafb90af4d79b9ac53a3f5ef13b0
jianhe <jianhe@39155.no-reply.drupal.org>
2014-01-17 12:44:23 -0500

    Issue #2167407 by jian he: Need give a chance for other modules to change the menu_link before save.

--- taxonomy_menu.api.php
+++ taxonomy_menu.api.php
@@ -7,6 +7,23 @@
  */
 
 /**
+ * Allows modules to alter the menu link before saved.
+ *
+ * @param $menu_link
+ *   The menu link will be saved.
+ * @param $term
+ *   The taxonomy term
+ * @param $menu_name
+ *   The machine name of the menu in which the menu link should be saved.
+ */
+function hook_taxonomy_menu_link_alter(&$menu_link, $term, $menu_name) {
+  // For example, change the link_title and options
+  $wrapper = entity_metadata_wrapper('taxonomy_term', $term);
+  $menu_link['link_title'] .= '<span class="tibetan">' . $wrapper->field_tibetan_name->value() . '</span>';
+  $menu_link['options']['html'] = TRUE;
+}
+
+/**
  * Allows modules to perform operations after a menu link resulting from the
  * processing of a taxonomy term has been saved.
  *
